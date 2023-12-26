504c6dca09c8b7ec20efe5fba690b1406616da9c
Laz <lazgerasimov@gmail.com>
2023-12-26 13:00:23 +0200

    add create new user webhook

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+import { CreateUserParams } from "@/types"
+import { handleError } from "../utils"
+import { connectToDatabase } from "../database"
+import User from "../database/models/user.module"
+
+
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+        await connectToDatabase();
+
+        const newUser = await User.create(user);
+
+        return JSON.parse(JSON.stringify(newUser));
+    } catch (error) {
+        handleError(error);
+    }
+}
